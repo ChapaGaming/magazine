389b55bee0beaaf138e7e48efda7e901c9df0b1c
ChapaGaming <159574595+ChapaGaming@users.noreply.github.com>
2024-12-26 13:39:33 +0300

    v 0.5
    Работа с базами данных: добавление товаров в "/admin/add", динамическое изменение(подгрузка товаров из базы данных) в "/" (домашней ссылке)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException, staticfiles, Request, Query, Depends
+from fastapi import FastAPI, File, UploadFile, HTTPException, staticfiles, Request, Query, Depends, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sqlmodel import Field, Session, SQLModel, create_engine, select
@@ -8,25 +8,27 @@ app = FastAPI(debug=True)
 my_host = "127.0.0.1"
 # функции
 def create_db_and_tables():# обнуление/создание таблиц
-    global acounts_engine,cataloge_engine
-    SQLModel.metadata.create_all(cataloge_engine)
-    SQLModel.metadata.create_all(acounts_engine)
+    global engine
+    SQLModel.metadata.create_all(engine)
     
 def get_session(): #создание сессии
     with Session(engine) as session:
         yield session
 
 # pydantic
-class Cataloge(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-    description: str | None
-    amount: int
 class Acounts(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     email: str
-    password: int
+    password: str
     basket: str|None
+
+class Cataloge(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    description: str
+    amount: int
+    cost: float
+
 
 # Указываем директорию для шаблонов
 templates = Jinja2Templates(directory="templates")
@@ -34,18 +36,11 @@ templates = Jinja2Templates(directory="templates")
 app.mount("/static", staticfiles.StaticFiles(directory="static"), name="static")
 
 # работа с SQL
-sqlite_file_name = "db/cataloge.db"
+sqlite_file_name = "db/database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
-cataloge_engine = create_engine(sqlite_url, connect_args=connect_args)
-####
-sqlite_file_name = "db/acounts.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-
-connect_args = {"check_same_thread": False}
-acounts_engine = create_engine(sqlite_url, connect_args=connect_args)
-
+engine = create_engine(sqlite_url, connect_args=connect_args)
 SessionDep = Annotated[Session, Depends(get_session)]
 
 # использование функций
@@ -54,10 +49,41 @@ SessionDep = Annotated[Session, Depends(get_session)]
 @app.on_event("startup")
 def on_start():
     create_db_and_tables()
-    
-@app.get("/", response_class=HTMLResponse)
+
+@app.get("/admin/add", response_class=HTMLResponse)
 async def read_root(request: Request):
     req = {"request": request}
+    return templates.TemplateResponse("creater.html", req)
+
+@app.post("/admin/add", response_class=HTMLResponse)
+async def read_root(session: SessionDep,tovar: Cataloge = Form(None)) -> Cataloge:
+    session.add(tovar)
+    session.commit()
+    session.refresh(tovar)
+    return HTMLResponse(content='<h1 style = "color: green;">успешно!</h1>')
+    
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request, session: SessionDep):
+    all = session.query(Cataloge).all()
+    names = list()
+    descriptions = list()
+    costs = list()
+    amounts = list()
+
+    for t in all:
+        amounts.append(t.amount)
+        names.append(t.name)
+        descriptions.append(t.description)
+        costs.append(t.cost)
+    req = {
+        "request": request,
+        "names": names,
+        "descriptions": descriptions,
+        "costs": costs,
+        "amounts": amounts,
+        "all": len(all)
+        }
     return templates.TemplateResponse("index.html", req)
 
 @app.get("/basket/", response_class=HTMLResponse)
@@ -71,11 +97,10 @@ async def basket(request: Request,user_id: int = -1):
     return templates.TemplateResponse("basket.html", req)
 
 @app.post("/")
-async def buying(request: Request):
-    form_data = await request.form()
-    form_type = form_data.get("buy_form")
-    print(form_type)
-    return form_data
+async def buying(session: SessionDep,request: Request, buy_form: int = Form(...)):
+    id = buy_form
+    tovar = session.get(Cataloge,id)
+    return tovar
     
 if __name__ == "__main__":
     import uvicorn
