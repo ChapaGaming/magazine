e931fc54df756baf4d656602fc244b1444045956
ChapaGaming <159574595+ChapaGaming@users.noreply.github.com>
2025-03-31 19:51:09 +0300

    новая
    оказывается надо сделать отсчёты, добавить количество товара одного типа отслеживать поступление новых автомабилей их перемежение, продажа и тд

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from sqlalchemy.pool import QueuePool
 from typing import Annotated
 from sqlalchemy import func
 import hashlib
+import datetime
+from colorama import init
+init()
+from colorama import Fore, Style
 
 #--------------------------#
 # venv\Scripts\activate    #
@@ -14,7 +18,6 @@ import hashlib
 #--------------------------#
 
 app = FastAPI(debug=True)
-global HOST, PORT
 # sqlmodel(pydantic)
 class UserCatalogeLink(SQLModel, table = True):
     user_id:  int | None = Field(default=None, foreign_key="user.id", primary_key=True)
@@ -22,28 +25,27 @@ class UserCatalogeLink(SQLModel, table = True):
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    username: str
+    FIO: str
     email: str
     password: str
-
+    seller: bool
     baskets: list["Cataloge"] = Relationship(back_populates="willings", link_model=UserCatalogeLink)#в " " так как питон интепритируемый и не увидет такого класса, а sqlalhemy увидит
 
 class Cataloge(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-    description: str
-    amount: int
+    model: str = Field(index=True)
+    color: str
+    in_stage: bool
     cost: float
-
+    buyer: str|None
+    kind: str
     willings: list[User] = Relationship(back_populates="baskets", link_model=UserCatalogeLink)
 
-# Указываем директорию для шаблонов
 
+# Указываем директорию для шаблонов
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", staticfiles.StaticFiles(directory="static"), name="static")
 
-#http://192.168.0.104:8000
-#https://magazine-4pqo.onrender.com/
 # работа с SQL
 def get_session(): #создание сессии
     with Session(engine) as session:
@@ -52,21 +54,34 @@ def get_session(): #создание сессии
 sqlite_file_name = "db/database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-connect_args = {"check_same_thread": False}
+connect_args = {"check_same_thread": False,}
 engine = create_engine(sqlite_url, poolclass=QueuePool, pool_size=10, max_overflow=20, pool_recycle=3600, connect_args=connect_args)
 SessionDep = Annotated[Session, Depends(get_session)]
 # функции связанные с fastapi
+def history(message : str,type_message : str = "Изменение", warning : bool = False) -> None:
+    if warning:
+        with open("db/history_main.txt", "a", encoding="utf-8") as history:
+            date = datetime.datetime.now()
+            history.write("\n"+type_message+" за "+str(date)+"\n")
+            history.write(message+"\n")
+    
+    with open("db/history.txt", "a", encoding="utf-8") as history:
+        date = datetime.datetime.now()
+        history.write("\n"+type_message+" за "+str(date)+"\n")
+        history.write(message+"\n")
+
 def go_login(session,email):
     
-    if 1==1:
+    try:
         statement = select(User).where(User.email == email)
         user = len(session.exec(statement).all())
         if user == 1:
             return False
         else:
             return True
-
-# функции не связанные с fastapi 
+    except:
+        return True
+# функции не связанные с fastapi
 def clear_db(db):
     with Session(engine) as session:
         results = session.exec(select(db)).all()
@@ -74,7 +89,7 @@ def clear_db(db):
             session.delete(result)
         session.commit()
 def hashing(text):
-    return hashlib.sha256(text.encode()).hexdigest()# текст кодируется по системе sha256 и превращается в 16-ричную систему
+    return hashlib.sha256(text.encode()).hexdigest()# текст кодируется по сиситеме sha256 и префращфется в 16-ричную систему
 
 def create_db_and_tables():# обнуление/создание таблиц
     global engine
@@ -83,15 +98,21 @@ def create_db_and_tables():# обнуление/создание таблиц
 def lifespan():
     create_db_and_tables()
 # обработка запросов
+@app.on_event("shutdown")
+def on_end():
+    with open("db/history.txt", "a", encoding="utf-8") as history:
+        date = datetime.datetime.now()
+        history.write("\n▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ \n Остановка сервера")
 @app.on_event("startup")
 def on_start():
     create_db_and_tables()
-@app.get("/login",response_class=HTMLResponse)
+    history("▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ","Запуск сервера")
+@app.get("/",response_class=HTMLResponse)
 def read_root(request: Request):
     req = {"request": request}
     return templates.TemplateResponse("login.html", req)
 
-@app.post("/login")
+@app.post("/")
 def read_root(session:SessionDep, request: Request, password: str|None = Form(...), email: str|None = Form(...)):
     hash_e =hashing(email)
     hash_pas = hashing(password)
@@ -99,7 +120,7 @@ def read_root(session:SessionDep, request: Request, password: str|None = Form(..
     result = session.exec(statement)
     first = result.first()
     if not (first is None):
-        return HTMLResponse(content=f"""<meta http-equiv="refresh" content="0.1; URL='/?email={hash_e}'" />""")
+        return HTMLResponse(content=f"""<meta http-equiv="refresh" content="0.1; URL='/cataloge?email={hash_e}'" />""")
     return HTMLResponse(content=f'<h1 style = "color: red;">Пользователь не найден</h1>')
 
 @app.get("/register",response_class=HTMLResponse)
@@ -108,14 +129,12 @@ def read_root(request: Request):
     return templates.TemplateResponse("register.html", req)
 
 @app.post("/register",response_class=HTMLResponse)
-def read_root(session: SessionDep, request: Request, username: str = Form(...), email: str = Form(...), password: str = Form(...), rep_password: str = Form(...)):
+def read_root(session: SessionDep, request: Request, FIO: str = Form(...), email: str = Form(...), password: str = Form(...), rep_password: str = Form(...)):
     if password == rep_password:
         try:
             hash_e = hashing(email)
             hash_pas = hashing(password)
-            user = User(username = username, email = hash_e, password = hash_pas, basket = None)
-        except ValidationError as e:
-            return e
+            user = User(FIO = FIO, email = hash_e, password = hash_pas, basket = None, seller=False)
         except Exception as e:
             return e
         email_query = select(User).where(User.email == hash_e)
@@ -126,33 +145,14 @@ def read_root(session: SessionDep, request: Request, username: str = Form(...), 
         session.add(user)
         session.commit()
         session.refresh(user)
-        return HTMLResponse(content=f"""<h1 style = "color: green;">Успешно</h1>> <meta http-equiv="refresh" content="2; URL='/login'" />""")
+        history("Пользователь {user} зарегистрировался","Регистрация")
+        return HTMLResponse(content=f"""<h1 style = "color: green;">Успешно</h1>> <meta http-equiv="refresh" content="2; URL='/'" />""")
     return f'<h1 style = "color: red;">Пароли не совпадают</h1>'
 
-@app.get("/admin/add", response_class=HTMLResponse)
-def read_root(request: Request):
-    req = {"request": request}
-    return templates.TemplateResponse("creater.html", req)
-
-@app.post("/admin/add", response_class=HTMLResponse)
-def read_root(session: SessionDep, description: str = Form(...), name: str = Form(...), amount: str = Form(...), cost: str = Form(...)):
-    try:
-        amount = float(amount.replace(",","."))
-        cost = float(cost.replace(",","."))
-        if cost > 0 and amount > 0 and amount % 1 == 0:
-            product = Cataloge(description=description, name=name.upper(), amount=amount, cost=cost)
-            session.add(product)
-            session.commit()
-            session.refresh(product)
-            return HTMLResponse(content=f"""<h1 style = "color: green;">Успешно</h1>> <meta http-equiv="refresh" content="2; URL='/login'" />""")
-        return HTMLResponse(content='<h1 style = "color: red;">Ошибочные данные</h1>')
-    except ValueError:
-        return HTMLResponse(content='<h1 style = "color: red;">Цена и количество не могут быть символами</h1>') 
-
 @app.get("/basket/", response_class=HTMLResponse)
-def basket(session: SessionDep, request: Request, email:str|None, searching: str | None = None):
+def read_root(session: SessionDep, request: Request, email:str|None, searching: str | None = None):
     if go_login(session,email):
-        return HTMLResponse(content=f"""<meta http-equiv="refresh" content="0.001; URL='/login'" />""")
+        return HTMLResponse(content=f"""<meta http-equiv="refresh" content="0.001; URL='/'" />""")
     req = {
         "request": request,
         "searching":"",
@@ -162,7 +162,7 @@ def basket(session: SessionDep, request: Request, email:str|None, searching: str
     statement = select(User).where(User.email == email)
     user = session.exec(statement).one()
     if searching and searching.strip() and user:
-        statement = select(Cataloge).where(Cataloge.name.like(f"%{searching.upper()}%"))
+        statement = select(Cataloge).where(Cataloge.model.like(f"%{searching.upper()}%"))
         alls = session.exec(statement).all()
         production = []
         for el in alls:
@@ -177,7 +177,22 @@ def basket(session: SessionDep, request: Request, email:str|None, searching: str
     req["len"] = len(production)
     return templates.TemplateResponse("basket.html", req)
 
-@app.get("/", response_class=HTMLResponse)
+
+@app.post("/basket", response_class=HTMLResponse)
+def read_root(session: SessionDep, email:str|None, submit: int = Form(...), searching: str | None = None):
+    if go_login(session,email):
+        return HTMLResponse(content=f"""<meta http-equiv="refresh" content="0.001; URL='/'" />""")
+    product = session.get(Cataloge,submit)
+    statement = select(User).where(User.email == email)
+    user = session.exec(statement).one()
+    user.baskets.remove(product)
+    session.add(user)
+    session.commit()
+    history(f"Пользователь {user.FIO} удалил отслеживание для {product}", "Удаление отслеживания")
+    return HTMLResponse(content=f"""<h1 style = "color: green;">Успешно</h1>> <meta http-equiv="refresh" content="0.5; URL='/cataloge?email={email}&searching={searching}'" />""")
+
+
+@app.get("/cataloge", response_class=HTMLResponse)
 async def read_root(request: Request, session: SessionDep, email: str | None = None, searching: str | None = None):
     req = {
         "request": request,
@@ -185,15 +200,17 @@ async def read_root(request: Request, session: SessionDep, email: str | None = N
         "searching":"",
         "production": [] # Устанавливаем production по умолчанию пустым списком
     }
-
-    if not go_login(session,email):
+    if go_login(session,email):
+        req["seller"] = False
+    else:
         statement = select(User).where(User.email == email)
         user = session.exec(statement).one()
-        req["user"] = user
-    else:
-        req["user"] = User(id = -1, username="None", email = "None", password="None")
+        req["seller"] = user.seller
+
+    
+    
     if searching and searching.strip():
-        statement = select(Cataloge).where(Cataloge.name.like(f"%{searching.upper()}%"))
+        statement = select(Cataloge).where(Cataloge.model.like(f"%{searching.upper()}%"))
         production = session.exec(statement).all()
         req["production"] = production
         req["searching"] = searching
@@ -201,42 +218,117 @@ async def read_root(request: Request, session: SessionDep, email: str | None = N
         statement = select(Cataloge)
         production = session.exec(statement).all()
         req["production"] = production
-        print(req["production"][0].willings)
     return templates.TemplateResponse("cataloge.html", req)
 
-@app.post("/", response_class=HTMLResponse)
-def buying(session: SessionDep,request: Request, email:str|None,id: int = Form(...),buy_form: str = Form(...), searching: str | None = None):
+@app.post("/cataloge", response_class=HTMLResponse)
+def read_root(session: SessionDep,request: Request, email:str|None, buy_form: int = Form(...), searching: str | None = None):
     if go_login(session,email):
-        return HTMLResponse(content=f"""<meta http-equiv="refresh" content="0.001; URL='/login'" />""")
+        return HTMLResponse(content=f"""<meta http-equiv="refresh" content="0.001; URL='/'" />""")
+    id = buy_form
+    product = session.get(Cataloge,id)
+    scalar = select(User).where(User.email == email)
+    result = session.exec(scalar)
+    if product.in_stage == False:
+        return HTMLResponse(content='<h1 style = "color: red;">Уже в диллерском центре</h1>')
+    user = result.first()
+    if user in product.willings:
+        return HTMLResponse(content='<h1 style = "color: red;">вы уже заказали, вы можете удалить его в списке отслеживания</h1>')
+    product.willings.append(user)
+    session.add(product)
+    session.commit()
     if not searching:
         searching=""
-    if buy_form == "buy":
-        product = session.get(Cataloge,id)
-        scalar = select(User).where(User.email == email)
-        result = session.exec(scalar)
-        print("Пользаватель добавил объект в корзину",product)
-        if product.amount < 1:
-            return HTMLResponse(content='<h1 style = "color: red;">нет в наличии</h1>')
-        user = result.first()
-        if user in product.willings:
-            return HTMLResponse(content='<h1 style = "color: red;">товар уже в корзине</h1>')
-        product.willings.append(user)
+    history(F"пользователь {user.FIO} начал отслеживать {product}", "Отслеживание")
+    return HTMLResponse(content=f"""<h1 style = "color: green;">Успешно</h1>> <meta http-equiv="refresh" content="0.5; URL='/cataloge?email={email}&searching={searching}'" />""")
+
+@app.get("/editor", response_class=HTMLResponse)
+def read_root(request: Request, session: SessionDep, email: str | None = None, searching: str | None = None):
+    req = {
+        "request": request,
+        "email": email,
+        "searching":"",
+        "production": [] # Устанавливаем production по умолчанию пустым списком
+    }
+    if go_login(session,email):
+        req["seller"] = False
+    else:
+        statement = select(User).where(User.email == email)
+        user = session.exec(statement).one()
+        req["seller"] = user.seller
+    
+    
+    if searching and searching.strip():
+        statement = select(Cataloge).where(Cataloge.model.like(f"%{searching.upper()}%")|Cataloge.in_stage == True)
+        production = session.exec(statement).all()
+        req["production"] = production
+        req["searching"] = searching
+    else:
+        statement = select(Cataloge).where(Cataloge.in_stage == True)
+        production = session.exec(statement).all()
+        req["production"] = production
+    return templates.TemplateResponse("editor.html", req)
+
+@app.post("/editor", response_class=HTMLResponse)
+def read_root(session: SessionDep,FIO_user: Annotated[str, Form()], email:str|None, acc_form: Annotated[str, Form()], kind: Annotated[str, Form()],  searching: str | None = None):
+
+    if go_login(session,email):
+        return HTMLResponse(content=f"""<meta http-equiv="refresh" content="0.001; URL='/'" />""")
+    mode = acc_form[-3:] # del удаление up_ изменение
+    id = acc_form[:-3]
+    product = session.get(Cataloge,id)
+    scalar = select(User).where(User.FIO == FIO_user)
+    user = session.exec(scalar)
+    user = user.first()
+
+    seller = select(User).where(User.email == email)
+    seller = session.exec(seller)
+    seller = seller.first()
+    if mode == "up_":
+        try:
+            user_name = user.FIO
+        except:
+            user_name = "<пусто>"
+        print(Fore.YELLOW + "WARNING"+ Style.RESET_ALL +f":  Продавец {seller.FIO}(id={seller.id}) обновил пользователю {user_name} товар:",product)
+        old_product = product
+        product.buyer = user_name
+        product.kind = kind
         session.add(product)
         session.commit()
-        return HTMLResponse(content=f"""<h1 style = "color: green;">Успешно</h1>> <meta http-equiv="refresh" content="0.005; URL='/?email={email}&searching={searching}'" />""")
-    elif buy_form == "sell":
-        product = session.get(Cataloge,id)
-        scalar = select(User).where(User.email == email)
-        user = session.exec(scalar).one()
-        if product in user.baskets:
-            user.baskets.remove(product)
-            session.add(user)
-            session.commit()
-            return HTMLResponse(content=f"""<h1 style = "color: green;">Успешно</h1>> <meta http-equiv="refresh" content="0.005; URL='/?email={email}&searching={searching}'" />""")
-        return HTMLResponse(content='<h1 style = "color: red;">товар уже убран из корзины</h1>')
+        if not searching:
+            searching=""
+        history(F"Продавец {seller.FIO} изменил {old_product} на {product}", "Обновление товара", warning=True)
+        return HTMLResponse(content=f"""<h1 style = "color: green;">Успешно</h1>> <meta http-equiv="refresh" content="0.5; URL='/editor?email={email}&searching={searching}'" />""")
+    elif mode == "del":
+        session.delete(product)
+        session.commit()
+        print(Fore.YELLOW + "WARNING"+ Style.RESET_ALL + f":  Продавец {seller.FIO}(id={seller.id}) удалил товар:", product)
+        history(F"Продавец {seller.FIO} удалил {product}", "Удаление", warning=True)
+        return HTMLResponse(content=f"""<h1 style = "color: green;">Успешно</h1>> <meta http-equiv="refresh" content="0.5; URL='/editor?email={email}&searching={searching}'" />""")
+
+
+@app.get("/add", response_class=HTMLResponse)
+def read_root(session: SessionDep, request: Request, email:str|None):
+    if go_login(session,email):
+        return HTMLResponse(content=f"""<meta http-equiv="refresh" content="0.001; URL='/'" />""")
+    return templates.TemplateResponse("add.html", {"request": request, "email": email})
+
+@app.post("/add", response_class=HTMLResponse)
+def read_root(session: SessionDep, request: Request, email:str|None, model: Annotated[str, Form()], kind: Annotated[str, Form()], in_stage: Annotated[str, Form()], cost: Annotated[int, Form()], color: Annotated[str, Form()]):
+    if go_login(session,email):
+        return HTMLResponse(content=f"""<meta http-equiv="refresh" content="0.001; URL='/'" />""")
+    seller = select(User).where(User.email == email)
+    seller = session.exec(seller)
+    seller = seller.first()
+    if in_stage == "1":
+        in_stage = True
     else:
-        return HTMLResponse(content='<h1 style = "color: red;">500 error</h1>')
-    
+        in_stage = False
+    item = Cataloge(model = model, kind = kind, in_stage=in_stage, color=color, cost=cost, willings=[])
+    history(F"пользователь {seller.FIO} создал {item}", "Создание", warning=True)
+    session.add(item)
+    session.commit()
+    return "200 OK"
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
